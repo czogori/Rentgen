97bd35f070ad5cde78a6efecec3daa21a3e6dd6c
Arek Jaskólski <arek.jaskolski@gmail.com>
2013-08-04 23:49:04 +0200

    Added new test file for AddColumnCommand class

---
+++ tests/Schema/Postgres/Manipulation/AddColumnCommandTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Rentgen\Tests\Schema\Postgres\Manipulation;
+
+use Rentgen\Schema\Postgres\Manipulation\AddColumnCommand;
+use Rentgen\Database\Table;
+use Rentgen\Database\Column;
+use Rentgen\Tests\TestHelpers;
+
+/**
+ * @author Arek Jaskólski <arek.jaskolski@gmail.com>
+ */
+class AddColumnCommandTest extends TestHelpers
+{
+    public function setUp()
+    {
+        $this->clearDatabase();
+    }
+
+    public function testGetSql()
+    {
+        $addColumnCommand = new AddColumnCommand();
+        $addColumnCommand->setTable(new Table('foo'));
+        $addColumnCommand->setColumn(new Column('name', 'string'));
+
+        $sql = 'ALTER TABLE foo ADD COLUMN name string;';
+        $this->assertEquals($sql, $addColumnCommand->getSql());
+    }
+
+    public function testExecute()
+    {        
+    }
+}
