d83cc76df1690b295be3cb095c13170b6221b73c
Arek Jaskólski <arek.jaskolski@gmail.com>
2013-08-10 23:42:17 +0200

    Added new test file for dropping constraint

---
+++ tests/Schema/Postgres/Manipulation/DropConstraintCommandTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Rentgen\Tests\Schema\Postgres\Manipulation;
+
+use Rentgen\Schema\Postgres\Manipulation\DropConstraintCommand;
+use Rentgen\Database\ForeignKey;
+use Rentgen\Database\Table;
+use Rentgen\Tests\TestHelpers;
+
+/**
+ * @author Arek Jaskólski <arek.jaskolski@gmail.com>
+ */
+class DropConstraintCommandTest extends TestHelpers
+{
+    public function setUp()
+    {
+        $this->clearDatabase();
+    }
+
+    public function testDropForeignKeyGetSql()
+    {
+    	$foreignKey = new ForeignKey(new Table('foo'), new Table('bar'));
+    	$foreignKey->setColumns('column_foo');
+
+        $dropConstraintCommand = new DropConstraintCommand();
+        $dropConstraintCommand->setConstraint($foreignKey);
+        $this->assertEquals('ALTER TABLE foo DROP CONSTRAINT foo_column_foo_fkey;', $dropConstraintCommand->getSql());
+    }
+}
