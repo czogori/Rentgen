50ad806893e9f427a5e9d7251be0cc469f4cc81e
Arek Jaskólski <arek.jaskolski@gmail.com>
2013-09-26 00:37:31 +0200

    Added comments

--- src/Rentgen/Database/Table.php
+++ src/Rentgen/Database/Table.php
@@ -8,16 +8,33 @@ class Table
     private $schemaName;
     private $columns = array();
 
+    /**
+     * Constructor.
+     * 
+     * @param string $name Table name.
+     */
     public function __construct($name)
     {
         $this->name = $name;
     }
 
+    /**
+     * Get table name.
+     * 
+     * @return string Table name.
+     */
     public function getName()
     {
         return $this->name;
     }
 
+    /**
+     * Add column to table.
+     * 
+     * @param Column $column Column instance.
+     *
+     * @return Table Table instance.
+     */
     public function addColumn(Column $column)
     {
         $this->columns[] = $column;
@@ -25,11 +42,22 @@ class Table
         return $this;
     }
 
+    /**
+     * Get columns of table.
+     * 
+     * @return Column[] Array of Column instances.
+     */
     public function getColumns()
     {
         return $this->columns;
     }
 
+    /**
+     * Get column of table.
+     * 
+     * @param  string $name Column name.
+     * @return Column       Column instance.
+     */
     public function getColumn($name)
     {
         foreach ($this->columns as $column) {
@@ -41,6 +69,11 @@ class Table
         return null;
     }
 
+    /**
+     * Get schema name.
+     * 
+     * @return string Schema name.
+     */
     public function getSchema()
     {
         return $this->schemaName;
