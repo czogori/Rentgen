697bf8f91740897dded8416e4efc59f27a3fe908
Arek Jaskólski <arek.jaskolski@gmail.com>
2013-10-31 00:27:58 +0100

    Added tests for Unique

--- tests/Database/Constraint/UniqueTest.php
+++ tests/Database/Constraint/UniqueTest.php
@@ -3,15 +3,31 @@
 namespace Rentgen\Tests\Database\Constraint;
 
 use Rentgen\Database\Table;
-use Rentgen\Database\Constraint\PrimaryKey;
+use Rentgen\Database\Constraint\Unique;
+use Rentgen\Database\Column\StringColumn;
 
 /**
  * @author Arek Jaskólski <arek.jaskolski@gmail.com>
  */
 class UniqueTest extends \PHPUnit_Framework_TestCase
 {
-    public function testCreateInstance()
-    {        
+	protected function setUp()
+	{
+    	$this->unique = new Unique(new Table('test'), array('foo', 'bar'));		
+	}
+    
+    public function testGetName()
+    {
+    	$this->assertSame('test_foo_bar_key', $this->unique->getName());	
     }
 
+    public function testGetColumns()
+    {
+    	$this->assertSame(array('foo', 'bar'), $this->unique->getColumns());
+    }
+
+    public function testGetTable()
+    {
+    	$this->assertEquals(new Table('test'), $this->unique->getTable());
+    }
 }
