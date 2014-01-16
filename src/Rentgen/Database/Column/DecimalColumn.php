a61f5be74e9e1d119655dfe3a09f36d8fac74998
Arek Jaskólski <arek.jaskolski@gmail.com>
2014-01-16 23:52:38 +0100

    Added funcionality for decimal column

--- src/Rentgen/Database/Column/DecimalColumn.php
+++ src/Rentgen/Database/Column/DecimalColumn.php
@@ -6,10 +6,46 @@ use Rentgen\Database\Column;
 
 class DecimalColumn extends Column
 {
+	private $precision;
+	private $scale = 0;
+
+	/**
+     * {@inheritdoc}
+     */
+	public function __construct($name, array $options = array())
+    {
+        parent::__construct($name, $options);
+
+        if (array_key_exists('precision', $options)) {
+            $this->precision = $options['precision'];
+	        if (array_key_exists('scale', $options)) {
+	            $this->scale = $options['scale'];
+	        }
+        }
+    }
+
     /**
-     * Get column type name.
+     * Gets a number precision.
      *
-     * @return string Column type name.
+     * @return integer
+     */
+	public function getPrecision()
+	{
+		return $this->precision;
+	}
+
+	/**
+     * Gets a number scale.
+     *
+     * @return integer
+     */
+	public function getScale()
+	{
+		return $this->scale;
+	}
+
+    /**
+     * {@inheritdoc}
      */
     public function getType()
     {
---
+++ tests/Database/Column/DecimalColumnTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Rentgen\Tests\Database;
+
+use Rentgen\Database\Column\DecimalColumn;
+
+/**
+ * @author Arek Jaskólski <arek.jaskolski@gmail.com>
+ */
+class DecimalColumnTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        $this->column = new DecimalColumn('foo');
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testCreateInstanceWithoutParams()
+    {
+        $column = new DecimalColumn();
+    }
+
+    public function testCreateInstance()
+    {
+        $this->assertInstanceOf('Rentgen\Database\Column', $this->column);
+        $this->assertInstanceOf('Rentgen\Database\Column\DecimalColumn', $this->column);
+    }
+
+    public function testGetName()
+    {
+        $this->assertEquals('foo', $this->column->getName());
+    }
+
+    public function testGetType()
+    {
+        $this->assertEquals('decimal', $this->column->getType());
+    }
+
+    public function testGetPrecision()
+    {
+        $column = new DecimalColumn('foo', array('precision' => 10));
+
+        $this->assertEquals(10, $column->getPrecision());
+    }
+
+    public function testGetScale()
+    {
+        $column = new DecimalColumn('foo', array('precision' => 10, 'scale' => 2));
+
+        $this->assertEquals(2, $column->getScale());
+    }
+
+    public function testDefaultPresionShouldBeUnknown()
+    {
+        $column = new DecimalColumn('foo');
+
+        $this->assertNull($column->getPrecision());
+    }
+
+    public function testDefaultScaleShouldBeZero()
+    {
+        $column = new DecimalColumn('foo', array('precision' => 10));
+
+        $this->assertEquals(0, $column->getScale());
+    }
+}
