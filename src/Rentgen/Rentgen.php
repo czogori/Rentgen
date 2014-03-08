e8344440236f4ada2ecd28a9452a5a3c96529e78
Arek Jaskólski <arek.jaskolski@gmail.com>
2014-03-08 02:14:01 +0100

    Added some comments

--- src/Rentgen/Rentgen.php
+++ src/Rentgen/Rentgen.php
@@ -12,6 +12,9 @@ class Rentgen
 {
     private $container;
 
+    /**
+     * Constructor.
+     */
     public function __construct()
     {
         $container = new ContainerBuilder();
@@ -24,26 +27,55 @@ class Rentgen
         $this->container = $container;
     }
 
+    /**
+     * Get the container.
+     *
+     * @return Symfony\Component\DependencyInjection\ContainerBuilder
+     */
     public function getContainer()
     {
         return $this->container;
     }
 
+    /**
+     * Get a service.
+     *
+     * @param string $service A service name.
+     *
+     * @return mixed
+     */
     public function get($service)
     {
         return $this->container->get($service);
     }
 
+    /**
+     * Create the manipulation instance.
+     *
+     * @return Rentgen\Schema\Manipulation
+     */
     public function createManipulationInstance()
     {
         return $this->container->get('rentgen.schema.manipulation');
     }
 
+    /**
+     * Create the info instance.
+     *
+     * @return Rentgen\Schema\Info
+     */
     public function createInfoInstance()
     {
         return $this->container->get('rentgen.schema.info');
     }
 
+    /**
+     * Execute sql query.
+     *
+     * @param string $sql A sql query.
+     *
+     * @return integer
+     */
     public function execute($sql)
     {
         $connection = $this->get('connection');
@@ -51,6 +83,13 @@ class Rentgen
         return $connection->execute($sql);
     }
 
+     /**
+     * Execute sql query with select intention.
+     *
+     * @param string $sql A sql query.
+     *
+     * @return array
+     */
     public function query($sql)
     {
         $connection = $this->get('connection');
