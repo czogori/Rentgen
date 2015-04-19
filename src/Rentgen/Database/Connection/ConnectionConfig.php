cd9cfb64f05d03769d4fa097342751aa0a7be93c
Arek Jaskólski <arek.jaskolski@gmail.com>
2015-04-20 00:36:15 +0200

    Refactoring database connection

--- src/Rentgen/Database/Connection/ConnectionConfig.php
+++ src/Rentgen/Database/Connection/ConnectionConfig.php
@@ -15,7 +15,7 @@ class ConnectionConfig implements ConnectionConfigInterface
      */
     public function __construct(array $config = array())
     {
-        $this->configure($config);
+        $this->parseConfiguration($config);
     }
 
     /**
@@ -60,41 +60,25 @@ class ConnectionConfig implements ConnectionConfigInterface
         return $this->adapter[$this->currentEnvironment];
     }
 
-    private function configure(array $config)
+    /**
+     * Parse configuration.
+     *
+     * @param array $config
+     * @return void
+     */
+    private function parseConfiguration(array $config)
     {
         $this->currentEnvironment = 'dev';
         foreach($config as $environment => $connection) {
-            if (isset($connection['dsn'])) {
-                $this->dsn[$environment]  = $connection['dsn'];
-                $connection = array_merge($connection, $this->parseDsn($connection['dsn']));
-            } else {
-                $this->dsn[$environment]= sprintf('%s:host=%s; port=%s; dbname=%s;'
-                    , $connection['adapter']
-                    , $connection['host']
-                    , $connection['port']
-                    , $connection['database']);
+            $this->dsn[$environment]= sprintf('%s:host=%s; port=%s; dbname=%s;',
+                $connection['adapter'],
+                $connection['host'],
+                $connection['port'],
+                $connection['database']);
 
-            }
             $this->adapter[$environment] = $connection['adapter'];
             $this->username[$environment] = $connection['username'];
             $this->password[$environment] = $connection['password'];
         }
-    }
-
-    /**
-     * Parse DSN string to arrray.
-     *
-     * @param string $dsn Dsn string.
-     *
-     * @return array
-     */
-    private function parseDsn($dsn)
-    {
-        $config = array();
-        if (preg_match('/^(.*):/', $dsn, $matches)) {
-            $config['adapter'] = $matches[1];
-        }
-
-        return $config;
     }
 }
